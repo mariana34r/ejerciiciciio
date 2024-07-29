3d244cf0d6890c1c16835c7c359fbb176350a2ac
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-07-29 13:03:09 -0500

    feat: :sparkles: funcion edit
    esta funcion edita las ciudades creadas
    
    @Kevinromero04

--- edit_city.py
+++ edit_city.py
@@ -1,10 +1,34 @@
 import carga_guarda as gg
 def menu():
-    menu = ["1. Para editar Nombre", "2. Para editar pais", "3. Para editar Poblacion",]
+    menu = ["1. Para editar Nombre", "2. Para editar pais", "3. Para editar Poblacion","4. Para salir"]
     for n in menu:
         print(n)
-menu()
+
     
 def edit_city():
-    data = gg.cargar_city()
-    cd_post = int(input())
+    while True:
+        data = gg.cargar_city()
+        cd_post = int(input("Digite el codigo postal de la ciudad que quieres editar: "))
+        if str(cd_post) in data["ciudades"]:
+            menu()
+            op = int(input("Seleccione una opcion: "))
+            
+            if op ==1:
+                nuevo = str(input("Digite el nuevo nombre de la ciudad: "))
+                data["ciudades"][str(cd_post)]["Nombre"] = nuevo
+                gg.guarda_city(data)
+                break
+            elif op ==2:
+                nuevo = str(input("Digite el nuevo nombre del pais: "))
+                data["ciudades"][str(cd_post)]["Pais"] = nuevo
+                gg.guarda_city(data)
+                break
+            elif op ==3:
+                nuevo = int(input("Digite la nueva poblacion: "))
+                data["ciudades"][str(cd_post)]["Poblacion"] = nuevo
+                gg.guarda_city(data)
+                break            
+            gg.guarda_city(data)
+        else:
+            print("No hay ninguna ciudad con este codigo postal")
+

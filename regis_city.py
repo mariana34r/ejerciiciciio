9c42f1fd32ab44546fa30c50202c89518d158a54
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-07-29 11:18:49 -0500

    fun_city

---
+++ regis_city.py
@@ -0,0 +1,15 @@
+import carga_guarda as gg
+
+def reg_city():
+    data = gg.cargar_rutas()
+    datos = {}
+    cd_postal = int(input("Digite el codigo postal de la ciudad que va a crear: "))
+    if str(cd_postal) in data["ciudades"]:
+        print("Ya existe una ciudad con este codigo postal")
+    datos["Nombre"] = str(input("Digite el nombre de la ciudad: "))
+    datos["Pais"] = str(input("Digite el pais donde se encuentra esta ciudad: "))
+    datos["Poblacion"] = int(input("Digite la poblacion de la ciudad: "))
+    data["ciudades"][cd_postal]=datos
+    gg.guarda_city(data)
+    
+reg_city()

e20a9040108fdcfd5d9cfd847b3a9e43ad1b2e83
mariana34r <cesaraugustoruedarangel@gmail.com>
2024-07-29 10:55:34 -0500

    menuu

---
+++ letreros.py
@@ -0,0 +1,9 @@
+def letrero1 (): 
+    print("-------------------")
+    print("---Administrador---")
+    print("-------------------")
+    
+def letrero2 (): 
+    print("------------------")
+    print("--- Usuario : )---")
+    print("------------------")
